7592af1a00c8fd761311673c1362530b13c07e68
rtb1c13 <R.T.Bradshaw@soton.ac.uk>
2015-11-09 09:46:07 +0000

    Update for fixed charge force calculations to print out energies for debug

--- Fields/fixed_fields.py
+++ Fields/fixed_fields.py
@@ -8,10 +8,25 @@
 #Usage: xxxxxxxxxxxxx
 
 import sander
+import sys,argparse
 import ParmedTools as parmed
 import numpy as np
 from chemistry import amber, unit as u
 
+### Argparser ###
+def parse():
+   parser = argparse.ArgumentParser()
+   parser.add_argument("-p","--parm",help="Path to Amber parameter file for analysis",type=str,required=True)
+   parser.add_argument("-c","--coords",help="Path to NetCDF trajectory file for analysis",type=str,required=True)
+   parser.add_argument("-a","--atoms",help="Atom numbers for analysis (Max 4)",nargs='+',type=int,required=True)
+   parser.add_argument("-m","--mask",help="Mask (in Amber atom mask format) for atoms to discharge whan calculating electrostatic forces (i.e. the 'ligand')",type=str,required=True)
+
+   if len(sys.argv)==1:
+      parser.print_help()
+      sys.exit(1)
+
+   args = parser.parse_args()
+   return args
 
 ### Class for atoms ###
 class Atom:
@@ -24,11 +39,11 @@ class Atom:
 
    def charge(self,prm):
       """Defines partial charge on atom num from prmtop"""
-#      listidx = args.atoms.index(num)
-#      if listidx > 1:
-#         return
-#      else:
-      self.chg = prm.orig.atoms[self.idx].charge
+      listidx = args.atoms.index(self.idx+1)
+      if listidx > 1:
+         return
+      else:
+         self.chg = prm.orig.atoms[self.idx].charge
 
 ### Class for Parmfile ###
 class Prmfile:
@@ -37,8 +52,9 @@ class Prmfile:
 
    def __init__(self,parmname):
       """Reads in Amber prmtop parmname."""
+
       self.orig = amber.AmberParm(parmname)
-      self.discharged = amber.AmberParm(parmname)
+      self.discharged = amber.AmberParm(parmname) # Bug in copy method = read in twice instead
 
    def discharge(self,atmmask):
       """Sets all charges of atoms NOT in provided
@@ -46,12 +62,9 @@ class Prmfile:
          Returns new, discharged, prmtop"""
 
       invmask = "!" + atmmask
-#      print invmask
-#      newparm = self.orig.copy(amber.AmberParm)
+#      print "invmask
       act = parmed.change(self.discharged, 'charge', invmask, 0.000)
       act.execute()
-#      print newparm
-#      self.discharged = newparm
 
 ###Class for trajectories###
 class Anal_traj:
@@ -69,11 +82,12 @@ class Anal_traj:
           1) X/Y/Z vector components
           2) Interatomic vector length
           3) X/Y/Z unit vector components."""
-      if len(atmlst) > 3:
+
+      if len(atmlst) > 3: # 4 atoms = cross product of 1,3,4
          atm1 = atmlst[0].idx
          atm2 = atmlst[2].idx
          atm3 = atmlst[3].idx
-      else:
+      else:               # 3 atoms = cross procuct of 1,2,3
          atm1 = atmlst[0].idx
          atm2 = atmlst[1].idx
          atm3 = atmlst[2].idx
@@ -95,17 +109,24 @@ class Anal_traj:
          self.leng[i] = np.linalg.norm(cross)
          self.unitvec[i] = cross / self.leng[i]
 
-   def sanderforce(self,parm,boxflag=False,pmeflag=False):
+   def sanderforce(self,parmstr,atmlst,boxflag=False,pmeflag=False):
       """Calculates energy and forces for a provided
-         Amber parm object and set of coordinates using the Sander
+         Amber parm string and set of coordinates using the Sander
          API. Also takes box dimensions and a flag for use of PME.
          Currently uses standard simulation options for gas phase or pme
          based on pmeflag.
-         Returns a sander energy object and a 3 x n array of forces with units"""
+         Only returns forces on first 2 atoms in atmlst
+         Returns a sander energy object and a 3 x 2 x traj length array
+         of forces with units"""
 
       self.forces=[]
       self.energies=[]
+#      sander.APPLY_UNITS = True # More bugs/inconsistencies in pysander in amber15, may be fixed later
+      indices = [a.idx for a in atmlst[:2]]
       if pmeflag is True:
+#        These default options will be suitable in most cases
+#        PME,cut=8.0,ntb=1,ntf=1,ntc=1
+#        Should be adapted if shake is required (not important here for ele)
          inp = sander.pme_input()
       else:
          inp = sander.gas_input()
@@ -113,18 +134,19 @@ class Anal_traj:
          coord = self.traj.coordinates(i)
          if boxflag is True:
             box = self.traj.box(i)
-#            print box
+#           print box
          else:
             box = None
-         with sander.setup(parm,coord,box=box,mm_options=inp):
-            ene,frc = sander.energy_forces()
-#         print i
+         with sander.setup(parmstr,coord,box=box,mm_options=inp): #Pass a string, else a temp parmfile is created that fills up tmp directory!
+            ene,frc = sander.energy_forces(as_numpy=False) # pysander __init__ bug/inconsistency, can't use as_numpy!
          frc = np.asarray(frc)
          frc = np.reshape(frc,((len(frc)/3.),3))
+         frcslice = frc[indices]
          # Add units for forces: kcal mol-1 A-1
-         frc = frc * u.kilocalorie / (u.mole * u.angstroms)
-         self.forces.append(frc)
+         frcslice = frcslice * u.kilocalorie / (u.mole * u.angstroms)
+         self.forces.append(frcslice)
          self.energies.append(ene)
+#      sander.APPLY_UNITS = False
 
 #Functions below here
 
@@ -154,11 +176,13 @@ def frc2field(frc,atmlst):
    fields = np.zeros((2,len(frc),3))
    for n in range(0,len(frc)):
       for i in range(0,3):
-         vec = frc[n][atmlst[0].idx][i]
+#         vec = frc[n][atmlst[0].idx][i]
+         vec = frc[n][0][i]
 	 fields[0][n][i] = vec.value_in_unit(u.megajoules/(u.centimeter*u.mole))
    for n in range(0,len(frc)):
       for i in range(0,3):
-         vec = frc[n][atmlst[1].idx][i]
+#         vec = frc[n][atmlst[1].idx][i]
+         vec = frc[n][1][i]
 	 fields[1][n][i] = vec.value_in_unit(u.megajoules/(u.centimeter*u.mole))
    # Divide through by Faraday constant to get fields in MV/cm
    # AND divide through by partial charge
@@ -166,37 +190,6 @@ def frc2field(frc,atmlst):
    fields[1] = fields[1]/(atmlst[1].chg * 96485.3329)
    return fields
 
-
-
-
-
-#import mdtraj as md
-#import numpy as np
-#import sys,argparse
-#from subprocess import call
-
-
-### Argparser ###
-#def parse():
-#   parser = argparse.ArgumentParser()
-#   parser.add_argument("-a","--atoms",help="Atom numbers for analysis (Max 4)",nargs='+',type=int,required=True)
-#   parser.add_argument("-ga","--gasatoms",help="Atom numbers for analysis (Max 4)",nargs='+',type=int)
-#   parser.add_argument("-t","--type",help="Type of analysis to run. 'dummy' will insert a dummy atom between atoms 1 & 2. 'standard' will evaluate fields at atoms 1 & 2. 'env' will take the difference between the fields in the solvated and gas (probe only) phase at atoms 1 & 2. Defaults to standard. ",choices=['dummy','standard','env'],default='standard')
-#   parser.add_argument("-p","--polarise",help="Polarisabilities for atoms 1 & 2. Defaults to carbonyl C and O.",nargs=2,type=float,default=['1.3340','0.8370'])
-#   parser.add_argument("-st","--solvtraj",help="Solvated trajectory file (Tinker arc, NO BOX INFO DUE TO BUG IN MDTRAJ)",type=str,required=True)
-#   parser.add_argument("-gt","--gastraj",help="Gas phase trajectory file (Tinker arc, NO BOX INFO DUE TO BUG IN MDTRAJ)",type=str)
-#   parser.add_argument("-sp","--solvprefix",help="Prefix for solvated coordinate files (Tinker xyz, with box info)",type=str)
-#   parser.add_argument("-gp","--gasprefix",help="Prefix for gas-phase coordinate files (Tinker xyz, no box info)",type=str)
-#
-#   if len(sys.argv)==1:
-#      parser.print_help()
-#      sys.exit(1)
-#
-#   args = parser.parse_args()
-#   return args
-
-
-# Projection along cross product of defined vectors
 def writefield_cross(fn,trajobj,field):
    """Writes out field projections in MV/cm 
       along unit vector at desired coordinate"""
@@ -207,84 +200,53 @@ def writefield_cross(fn,trajobj,field):
       outfile.write("%8.3f \n" % fieldproj)
    outfile.close()
 
-#def env_field():
-#   """Analyses field at two atoms Correction is made for
-#      the self field of the probe. Field is projected along
-#      internuclear vector or cross product of two related bonds"""
-#   print "Reading in trajectory %s as the solvated system and %s as the gas system" % (args.solvtraj, args.gastraj)
-#   arcname = args.solvtraj
-#   arcname_gas = args.gastraj
-#   arc = Anal_traj(arcname)
-#   gasarc = Anal_traj(arcname_gas)
-#   print "Calculating field at atoms %d and %d." % (args.atoms[0],args.atoms[1])
-#   atmlst=[]
-#   gatmlst=[]
-#   for i in args.atoms:
-#      j = Atom(i)
-#      j.polarise(i)
-#      atmlst.append(j)
-#   for i in args.gasatoms:
-#      j = Atom(i)
-#      j.polarisegas(i)
-#      gatmlst.append(j)
-#   # Decide on where to project according to length of atom list
-#   if len(atmlst) == 2:
-#      print "Projecting along internuclear vector %d to %d" % (args.atoms[0],args.atoms[1])
-#      arc.getcoords(atmlst)
-#      arc.vectors()
-#      gasarc.getcoords(gatmlst)
-#      gasarc.vectors()
-#   elif len(atmlst) == 3:
-#      print "Projecting along cross product %d to %d x %d to %d" % (args.atoms[0],args.atoms[1],args.atoms[0],args.atoms[2])
-#      arc.cross(atmlst)
-#      gasarc.cross(gatmlst)
-#   elif len(atmlst) == 4:
-#      print "Projecting along cross product %d to %d x %d to %d" % (args.atoms[0],args.atoms[2],args.atoms[0],args.atoms[3])
-#      arc.cross(atmlst)
-#      gasarc.cross(gatmlst)
-#   # Get field
-#   gas = False
-#   solfield = analyze_dipl_detailed(args.solvprefix,arc,atmlst,gas)
-#   gas = True
-#   gasfield = analyze_dipl_detailed(args.gasprefix,gasarc,gatmlst,gas)
-#   field = solfield - gasfield
-#   writefield(("field_at_atom_%i.txt" % args.atoms[0]),arc,field[0])
-#   writefield(("field_at_atom_%i.txt" % args.atoms[1]),arc,field[1])
+def debug_energies(fn,traj):
+   """Prints out energies calculated internally
+      using pysander API for comparison with those
+      from sander standalone.
+
+      Note: These will NOT agree with energies in 
+      simulation output files - output file energies
+      are a half-timestep removed from coordinates!
+      Rerun sander with imin=5 to obtain correct energies
+      for frames in coordinate file."""
+
+   outfile = open(fn,'w')
+   keylist = ["Tot","Bond","Angle","Dihedral","Elec","Elec 1-4","VDW","VDW 1-4"]
+   row_format = "{:>12}" * len(keylist)
+   outfile.write(row_format.format(*keylist)+"\n")
+   row_format = "{:>12.4f}" * len(keylist)
+   for e in traj.energies:
+      enelist = [e.tot,e.bond,e.angle,e.dihedral,e.elec,e.elec_14,e.vdw,e.vdw_14]
+      outfile.write(row_format.format(*enelist)+"\n") 
+   outfile.close()
 
 ### MAIN BELOW HERE ###
-#def main():
-#
-#   global args
-#   global gas
-#   args = parse()
-#   if args.type == "dummy":
-#      dummy_field()
-#   elif args.type == "standard":
-#      standard_field()
-#   elif args.type == "env":
-#      env_field()
+def main():
 
+   global args
+   args = parse()
+   prmfile = args.parm
+   trajfile = args.coords
 
-prmfile = "../1M9C_solv_ff14SB.prmtop"
-trajfile = "../1M9C_run1.mdcrd"
-
-parameters = Prmfile(prmfile)
-parameters.discharge(":164-169")
-orig_trajectory = Anal_traj(trajfile)
-new_trajectory = Anal_traj(trajfile)
-atoms = [2507,2505,2517,2508]
-for i in range(0,len(atoms)):
-  atoms[i] = Atom(atoms[i])
-  atoms[i].charge(parameters)
-# Work out original forces
-orig_trajectory.sanderforce(parameters.orig,boxflag=True,pmeflag=True)
-new_trajectory.sanderforce(parameters.discharged,boxflag=True,pmeflag=True)
-elefrc = subtract_frcs(orig_trajectory.forces,new_trajectory.forces)
-#print elefrc[0][atoms[0].idx]
-fields = frc2field(elefrc,atoms)
-orig_trajectory.cross(atoms)
-writefield_cross("atom_2507.out",orig_trajectory,fields[0])
-writefield_cross("atom_2505.out",orig_trajectory,fields[1])
+   # Assign params and trajectories
+   parameters = Prmfile(prmfile)
+   parameters.discharge(args.mask)
+   parameters.discharged.write_parm("discharged.parm7")
+   orig_trajectory = Anal_traj(trajfile)
+   new_trajectory = Anal_traj(trajfile)
+   atoms = [Atom(i) for i in args.atoms]
+   [i.charge(parameters) for i in atoms]
+   # Work out original forces
+   orig_trajectory.sanderforce(args.parm,atoms,boxflag=True,pmeflag=True)
+   new_trajectory.sanderforce("discharged.parm7",atoms,boxflag=True,pmeflag=True)
+   debug_energies("Energies.txt",orig_trajectory)
+   elefrc = subtract_frcs(orig_trajectory.forces,new_trajectory.forces)
+   # Fields and projection
+   fields = frc2field(elefrc,atoms)
+   orig_trajectory.cross(atoms)
+   writefield_cross("field_at_%i.txt" % args.atoms[0],orig_trajectory,fields[0])
+   writefield_cross("field_at_%i.txt" % args.atoms[1],orig_trajectory,fields[1])
 
 ############################################
-#main()
+main()
