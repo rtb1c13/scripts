7aae4c0a4418cbd5bfebfe5bc05f0d3e90e1eb68
rtb1c13 <R.T.Bradshaw@soton.ac.uk>
2016-02-23 12:12:07 +0000

    Deby_differences with argparser

--- SAXS_MD/Debye_differences.py
+++ SAXS_MD/Debye_differences.py
@@ -22,13 +22,28 @@ import matplotlib.cm as cm
 
 # argparser
 
+def parse():
+   parser = argparse.ArgumentParser()
+   parser.add_argument("-p","--parm",help="Parameter/topology file for desired structure",type=str,required=True)
+   parser.add_argument("-t","--traj",help="Coordinate file for desired structure",type=str,required=True)
+   parser.add_argument("-ff","--formfactor",help="Formfactor file for desired structure. Note that formfactors for Hydrogens should be included in their attached heavy atoms. This script does not currently support explicit hydrogens",type=str,required=True)
+   parser.add_argument("-f","--frames",help="Frames to analyse. If you supply one frame here, a SAXS profile will be generated. Two frames will generate a difference plot. Three frames will be used as the start, stop & stride values through a trajectory, and generate a profile for each one (WARNING, TIME CONSUMING!). Defaults to calculating the profile of the first frame",nargs='+',type=int,default=1)
+   parser.add_argument("-q","--qvalues",help="List of Q-values for analysis. Defaults to 0.1",nargs='*',type=float,default=[0.1])
+   parser.add_argument("-s","--select",help="Selection string for atoms to analyse from trajectory (in CHARMM/VMD format). Formfactor file should be pre-processed to only include these atoms.",nargs='*',type=str,default='protein and not name H*')
+ 
+
+   if len(sys.argv)==1:
+      parser.print_help()
+      sys.exit(1)
+
+   args = parser.parse_args()
+   return args
+
 
 # Exception
 
 class QCalc_Error(Exception):
    """Exception in Debye differences module"""
-
-
 
 # Define MDanalysis universe
 
@@ -41,12 +56,12 @@ def setup_universe(parm,traj,fformat='NCDF'):
       Returns MDAnalysis Universe object"""
    return Universe(parm,traj,format=fformat)
 
-def atoms_slice(univ,atoms='protein'): # Could probably do this more cleverly to allow index based slicing options. Ok for now.
+def atoms_slice(univ,atoms='protein and not name H*'): # Could probably do this more cleverly to allow index based slicing options. Ok for now.
    """Sets up a group of atoms on which to perform later analysis.
       Usage: atoms_slice(universe,[atoms])
       Requires a universe object, and an optional string in CHARMM selection format
-      (this is the same as you'd use in VMD). Default is 'protein'.
-      Returns MDAnalsyis AtomGroup"""
+      (this is the same as you'd use in VMD). Default is 'protein and not name H*'.
+      Returns MDAnalysis AtomGroup"""
    return univ.select_atoms(atoms)
 
 def get_trajslice(univ,start=0,end=None,stride=1): # For looping through all conformations
@@ -79,23 +94,23 @@ def get_coords(atoms,timestep=None):
          raise QCalc_Error("Your atoms don't have a method associated to get their positions. You're probably trying to get the intensity due to a single atom. This won't work with this script")
 
 # Matrix generation
-def gen_matrix(i1,atoms):
-   """Creates symmetrical matrix from input array of length N(N-1/2).
-      Atoms read from atom group provided."""
+#def gen_matrix(i1,atoms):
+#   """Creates symmetrical matrix from input array of length N(N-1/2).
+#      Atoms read from atom group provided."""
+#
+#   addedvals = len(atoms)-1
+#   curridx = 0
+#   matx = np.zeros((len(atoms),len(atoms)))
+#   for i in xrange(len(atoms)):
+#      matx[i][i+1:] = i1[curridx:curridx+addedvals]
+#      curridx += addedvals
+#      addedvals -= 1
+#   matx = matx + matx.T # Add its own transpose to get symmetry
+#   return matx
 
-   addedvals = len(atoms)-1
-   curridx = 0
-   matx = np.zeros((len(atoms),len(atoms)))
-   for i in xrange(len(atoms)):
-      matx[i][i+1:] = i1[curridx:curridx+addedvals]
-      curridx += addedvals
-      addedvals -= 1
-   matx = matx + matx.T # Add its own transpose to get symmetry
-   return matx
 
 
-
-# Object for a single Q-value
+# QPoint class for a single Q-value
 class QPoint:
 
    def __init__(self,q):
@@ -134,84 +149,129 @@ class QPoint:
       return df
    
    def calc_intensities(self,dist):
-      """Calculates and returns intensities at this Q value from an array of rij distances."""
- # Need to put tests for size/shape consistency etc in here
+      """Calculates and returns intensities at this Q value from an array of
+         rij distances."""
       debyearr = np.apply_along_axis(self.apply_debye,1,dist)
       try:
          debyearr = debyearr * np.exp(-0.23*(self.q**2)) * self._ffarray # Modulation parameter
       except ValueError, err:
           print err
-          raise QCalc_Error("Error in calculating intensities. Your form factors or distance matrix may be calculated incorrectly. See message above from Numpy")
+          raise QCalc_Error("""Error in calculating intensities. Your 
+                               form factors or distance matrix may be 
+                               calculated incorrectly. See message above
+                               from Numpy""")
       return debyearr
 
-def gen_diffmatrix(i1,i2,atoms):
-   """Creates difference matrix from two intensity arrays"""
+# Conformer class for single frames of a trajectory
+class Conformer:
+   """Class to hold information for SAXS analysis of single structure"""
+   def __init__(self,univ,frame=None,**kwargs):
+      self.atms = atoms_slice(univ,**kwargs)
+      if frame is not None:
+         self.coords = get_coords(self.atms,u.trajectory[frame]) 
+      else:
+         self.coords = get_coords(self.atms)
+      self.intensities={}
+      self.label = u.trajectory.frame
 
-   diffs = i1 - i2
-#   addedvals = len(atoms)-1
-#   curridx = 0
-#   diffmatrix = np.zeros((len(atoms),len(atoms)))
-#   for i in xrange(len(atoms)):
-#      diffmatrix[i][i+1:] = diffs[curridx:curridx+addedvals]
-#      curridx += addedvals
-#      addedvals -= 1
-   return diffs
+   def _gen_matrix(self):
+      """Creates symmetrical matrix from input array of length N(N-1/2).
+         Atoms read from atom group of current object."""
 
-def gen_matrix(i1,atoms):
-   """Creates symmetrical matrix from input array of length N(N-1/2).
-      Atoms read from atom group provided."""
+      addedvals = len(self.atms)-1
+      curridx = 0
+      matx = np.zeros((len(self.atms),len(self.atms)))
+      for i in xrange(len(self.atms)):
+         matx[i][i+1:] = self._dists[curridx:curridx+addedvals]
+         curridx += addedvals
+         addedvals -= 1
+      matx = matx + matx.T # Add its own transpose to get symmetry
+      return matx
 
-   addedvals = len(atoms)-1
-   curridx = 0
-   matx = np.zeros((len(atoms),len(atoms)))
-   for i in xrange(len(atoms)):
-      matx[i][i+1:] = i1[curridx:curridx+addedvals]
-      curridx += addedvals
-      addedvals -= 1
-   matx = matx + matx.T # Add its own transpose to get symmetry
-   return matx
+   def get_distmatrix(self,**kwargs):
+      """Generates a distance matrix using the self_distance_array function
+         of MDAnalysis."""
+
+      self._dists = distanal.self_distance_array(self.coords,**kwargs) # No PBC necessary if 
+                                                                       # trajectory already wrapped
+      self.distmatx = self._gen_matrix()
 
 
-
+# Functions for plotting
 def absmax(nparr):
    """Returns the absolute maximum value from a numpy array"""
    _ = np.absolute(nparr)
    return np.max(_)
 
-#### main below here
+def write_profile(conf,qmin=0.01,qmax=0.50,step=0.01):
+   """Writes a SAXS profile of a given conformer to file.
+      Usage: write_profile(Conformer,[qmin],[qmax],[step])
+      Requires Conformer object as input, by default prints out
+      profile between Q=0.01 and Q=0.50"""
 
-u = setup_universe('6G08_box.prmtop','6G08_allruns.mdcrd')
-prot = atoms_slice(u,atoms='protein and not name H*')
-#prot = prot.CA # Edit to only do C-alphas
-conf1 = get_coords(prot,u.trajectory[94]) # edit for 95ns frame
-conf2 = get_coords(prot,u.trajectory[576]) # edit for 577ns frame
-d1 = distanal.self_distance_array(conf1) # No PBC necessary if trajectory already wrapped
-d1 = gen_matrix(d1,prot)
-d2 = distanal.self_distance_array(conf2)
-d2 = gen_matrix(d2,prot)
+   for q in np.arange(qmin,qmax+step,step):
+      try:
+         conf.intensities[q]
+         continue
+      except KeyError:
+         Q1 = QPoint(q)
+         Q1.get_ff(args.formfactor,conf.atms)
+         conf.intensities[q] = Q1.calc_intensities(conf.distmatx)
+   with open(args.traj+'.dat','a') as f:
+      for q in np.arange(qmin,qmax+step,step):
+         f.write('%5.3f %12.6f\n' % (q,np.sum(conf.intensities[q])))
 
-for qval in [0.1,0.12,0.14,0.16,0.18,0.2,0.22,0.24,0.26,0.28,0.3]:
-#   qval = qval*0.01
-   Q1 = QPoint(qval)
-   Q1.get_ff('6G08_run1_stripped.ff',prot)
-   intensity1 = Q1.calc_intensities(d1)
-   intensity2 = Q1.calc_intensities(d2)
-   print 'Q = %s, intensity at frame 95 = %s' % (qval,np.sum(intensity1))
-#   with open('95_plot_mdcrd.dat','a') as f:
-#      f.write('%s %s\n' % (qval,np.sum(intensity1)))
-   print 'Q = %s, intensity at frame 577 = %s' % (qval,np.sum(intensity2))
-#   with open('577_plot.dat','a') as f2:
-#      f2.write('%s %s\n' % (qval,np.sum(intensity2)))
-
-   matx = gen_diffmatrix(intensity1,intensity2,prot)
-#   matx = matx + matx.T # Add two semi-populated halves together (matrix plus its transpose)
-
-   # Matplotlib fluff
-   arrmax = absmax(matx)
+def plot_diffmatrix(ref,probe,q=0.1):
+   """Plots 2D difference matrix for given Conformer objects
+      at desired Q-value (default 0.1).
+      Usage: plot_diffmatrix(Conformer1,Conformer2,[q])"""
+   try:
+      diffs = ref.intensities[q] - probe.intensities[q]
+   except ValueError, err:
+      print err
+      raise QCalc_Error("""Your intensity arrays are different sizes - 
+                           are you sure your structures/atom selections
+                           are the same size? See Numpy error above.""")
+   except KeyError:
+      raise QCalc_Error("""One or both of your conformers is missing an 
+                           intensity matrix.""")
+   arrmax = absmax(diffs)
    arrmin = arrmax*-1
    plt.figure()
-   plt.imshow(matx,interpolation='None',vmin=arrmin/2,vmax=arrmax/2,cmap=cm.RdBu,extent=[1,len(prot),len(prot),1])
-   plt.title('NoH_diffs frames 95-577 Q=%s' % qval)
+   plt.imshow(diffs,interpolation='None',vmin=arrmin,vmax=arrmax,cmap=cm.RdBu)
+#   plt.gca().set_xticks(range(1,len(ref.atms)+1,200))
+#   plt.gca().set_yticks(range(len(ref.atms),0,-200))
+   plt.title('Intensity difference matrix, frames %s-%s Q=%s' % (ref.label,probe.label,q))
    plt.colorbar()
-   plt.savefig('NoH_diffs_95-577_%s.png' % qval,bbox_inches='tight',dpi=300)
-   plt.show()
+   plt.savefig('Diffs_%s-%s_%s.png' % (ref.label,probe.label,q),bbox_inches='tight',dpi=300)
+      
+
+#### main below here
+global args
+args = parse()
+u = setup_universe(args.parm,args.traj)
+if len(args.frames) == 1:                   # Profile only
+   conf1 = Conformer(u,frame=args.frames[0]-1,atoms=args.select)
+   conf1.get_distmatrix()
+   write.profile(conf1)
+elif len(args.frames) == 2:                 # Difference matrix
+   conflist = []
+   for conf in args.frames:
+      currconf = Conformer(u,frame=conf,atoms=args.select) 
+      currconf.get_distmatrix()
+      for q in args.qvalues:
+         Q1 = QPoint(q)
+         Q1.get_ff(args.formfactor,currconf.atms)
+         currconf.intensities[q] = Q1.calc_intensities(currconf.distmatx)
+      conflist.append(currconf)
+   for q in args.qvalues:
+      plot_diffmatrix(conflist[0],conflist[1],q)
+else:                                       # Trajectory slice
+   trajslice = get_trajslice(u,args.frames[0],args.frames[1],args.frames[2])
+   for frame in trajslice:
+      conf1 = Conformer(u,frame=args.frames[0]-1,atoms=args.select)
+      conf1.get_distmatrix()
+      write.profile(conf1)
+      
+
+      
