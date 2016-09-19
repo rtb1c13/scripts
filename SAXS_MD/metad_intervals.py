9ff673ee4b9885a8546505d1b098569520abebbd
rtb1c13 <R.T.Bradshaw@soton.ac.uk>
2016-09-19 18:03:05 +0100

    Updated metad_intervals with option to adjust to reference struct

--- SAXS_MD/metad_intervals.py
+++ SAXS_MD/metad_intervals.py
@@ -16,6 +16,7 @@ def parse():
    parser.add_argument("-q","--qvalues",help="List of Q-values for analysis. Defaults to 0.1",nargs='*',type=float,default=[0.1])
    parser.add_argument("-o","--output",help="Filename for output. Defaults to 'plumed_outputs.txt'",type=str,default="plumed_outputs.txt")
    parser.add_argument("-s","--scale",help="Scale factor for width of MetaD interval. Multiplied by the relative experimental error at each Q-value. Defaults to 3.0",type=float,default=3.0)
+   parser.add_argument("-a","--adjust",help="Adjust MetaD intervals using a reference set of intensities (the second entry in the plumed intensity files. Defaults to False",type=bool,default=False)
 
    if len(sys.argv)==1:
       parser.print_help()
@@ -29,12 +30,15 @@ class Profile:
    """Class to handle reading/writing (TBC) of 
       plumed-calculated SAXS profiles"""
 
-   def __init__(self,fn,qvals=50):
+   def __init__(self,fn,qvals=50,ref=False):
       """Reads in plumed SAXS profile of given filename
          and number of Q-values (default 50). Usage:
          Profile(filename,[qvals=50])"""
       self._inpfile = fn
       self.plumed = np.genfromtxt(fn,skip_header=1,max_rows=qvals,dtype="float32")
+      if ref:
+         self.xtal = np.genfromtxt(fn,skip_header=3+qvals,max_rows=qvals,dtype="float32")
+         
 
    def get_expt(self,fn):
       """Reads in experimental profile for given Plumed profile
@@ -62,41 +66,59 @@ class Profile:
       return relatives[idx2] - (grad*qdiffs[idx2])
 
 # Interpolate an intensity from the two nearest values in a Q/intensity array 
-   def _itplint(self,val):
+   def _itplint(self,arr,val):
       """Interpolate a relative error from the two nearest values in
          experimental Q/intensity/error array"""
-      qdiffs = self.plumed[:,0]-val
-      idx1 = np.argmax(qdiffs[qdiffs<0]) # -ve value closest to 0
+      qdiffs = arr[:,0]-val
+      try:
+         idx1 = np.argmax(qdiffs[qdiffs<0]) # -ve value closest to 0
+      except ValueError: # No negative values
+         if np.min(qdiffs) == 0:
+            idx1 = np.argmin(qdiffs)
+         else:
+            raise ValueError("No intensity values at Q < %4.3f. Interpolation not possible" % val)
+            sys.exit(1) 
       idx2 = idx1 + 1
-      y = self.plumed[idx2,1] - self.plumed[idx1,1]
+      y = arr[idx2,1] - arr[idx1,1]
       x = qdiffs[idx2] - qdiffs[idx1]
       grad = y/x
-      return self.plumed[idx2,1] - (grad*qdiffs[idx2])
+      return arr[idx2,1] - (grad*qdiffs[idx2])
 
-   def get_rel_err(self,qvals):
+   def get_rel_err(self,qvals,adj_intervals=False):
       """Calculates the relative error in a plumed intensity at a
          given Q value from an associated experimental dataset"""
       self.relatives = []
       for q in qvals:
-         r_int = self._itplint(q)
+         r_int = self._itplint(self.plumed,q)
          r_err = self._itplerr(q)
          self.relatives.append([q,r_int*r_err])
       self.relatives.sort()
 
-   def write_plumed(self,fn="plumed_outputs.txt",sc=3):
+   def write_plumed(self,fn="plumed_outputs.txt",sc=3,adj_intervals=False):
       """Writes plumed inputs at desired Q values, scaled
          according to scale factor""" 
       for _ in self.relatives:
-         with open(fn,'a') as wf:
-            wf.write("Q=%5.3f INTERVAL=%5.3f,%5.3f GRID_MIN=%5.3f GRID_MAX=%5.3f GRID_BIN=1000\n"
-                     % (_[0],-sc*_[1],sc*_[1],-sc*1.2*_[1],sc*1.2*_[1]))
+         if adj_intervals:
+            ref_int = self._itplint(self.xtal,_[0])
+            r_int = self._itplint(self.plumed,_[0])
+            with open(fn,'a') as wf:
+               wf.write("Q=%5.3f INTERVAL=%5.3f,%5.3f GRID_MIN=%5.3f GRID_MAX=%5.3f GRID_BIN=1000\n"
+                        % (_[0],-sc*_[1] - (r_int-ref_int),
+                        sc*_[1] - (r_int-ref_int),
+                        -sc*1.2*_[1] - (r_int-ref_int),
+                        sc*1.2*_[1] - (r_int-ref_int)))
+         else:
+            with open(fn,'a') as wf:
+               wf.write("Q=%5.3f INTERVAL=%5.3f,%5.3f GRID_MIN=%5.3f GRID_MAX=%5.3f GRID_BIN=1000\n"
+                        % (_[0],-sc*_[1],sc*_[1],-sc*1.2*_[1],sc*1.2*_[1]))
+            
 
 ### Write out a series of Plumed inputs ###
 def generate_plumed_input():
-   prof = Profile(args.plumed,args.length)
+   prof = Profile(args.plumed,args.length,ref=args.adjust)
    prof.get_expt(args.expt)
    prof.get_rel_err(args.qvalues)
-   prof.write_plumed(args.output,args.scale)
+   prof.write_plumed(args.output,args.scale,adj_intervals=args.adjust)
 
 
 ### Main below here ###
